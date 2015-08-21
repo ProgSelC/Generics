3883732d08b057cbe7def422041b001de4099fa0
ProgSelC <ProgSelC@users.noreply.github.com>
2015-08-22 01:09:41 +0300

    Create Main.java

---
+++ mapTask3/Main.java
@@ -0,0 +1,31 @@
+package selcpkg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) {
+		Integer[] a = { 1, 1, 2, 3, 3, 4, 5, 6, 7, 9, 9, 9, 9, 9, 10 };
+		
+		typeDuplicates(a);
+
+	}
+
+	public static <T> void typeDuplicates(T[] a) {
+		HashMap<T, Integer> map = new HashMap<T, Integer>();
+		for (T key : a) {
+			if (map.get(key) == null) {
+				map.put(key, 1);
+			} else {
+				map.put(key, map.get(key) + 1);
+			}
+		}
+
+		for (Map.Entry<T, Integer> s : map.entrySet()) {
+			if (s.getValue() > 1) {
+				System.out.printf("Element \"%s\" has: %2d entries\n", s.getKey(), s.getValue());
+			}
+		}
+	}
+
+}
